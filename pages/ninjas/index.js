643c5d068880cfc90f3248bcb43b5c5f55fbf2e1
Aldrin Jenson <aldrinjenson@gmail.com>
2022-01-21 01:15:58 +0530

    Add getStaticProps for fetching data

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,27 @@
-import React from "react";
+import styles from "../../styles/Ninjas.module.css";
+import Link from "next/link";
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  // this function runs during build time only and never in the browser
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  // console.log(data); // can't get data in browser console
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 };
